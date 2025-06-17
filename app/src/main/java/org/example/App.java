e97956aad0dd2959a7f3151e6e2fbcd8a8300038
pb86286 <pb86286@mail.harpercollege.edu>
2025-06-17 15:22:48 -0500

    code

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,5 +3,7 @@ package org.example;
 public class App {
   public static void main(String[] args) {
     // Your code goes here
+    HelloWorld helloworld = new HelloWorld();
+    System.out.println(helloworld.getGreeting());
   }
 }
--- app/src/main/java/org/example/HelloWorld.java
+++ app/src/main/java/org/example/HelloWorld.java
@@ -2,4 +2,7 @@ package org.example;
 
 public class HelloWorld {
   // Your code goes here
+  public String getGreeting() {
+    return "Hello World!";
+  }
 }
